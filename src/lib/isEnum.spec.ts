6c17c942432ccf18d456d21d356fef9eeba6bb19
Gabriel Rocha de Oliveira <rolivegab@gmail.com>
2022-05-07 10:49:39 -0300

    feat: isEnum

--- src/lib/isEnum.spec.ts
+++ src/lib/isEnum.spec.ts
@@ -1,1 +1,30 @@
-describe("isEnum", () => {});
+import { isEnum } from "./isEnum";
+
+enum NumberEnum {
+  A,
+  B,
+  C,
+  D = 247,
+}
+
+enum StringEnum {
+  A = "a",
+  B = "b",
+  C = "QWE",
+}
+
+describe("isEnum", () => {
+  it("correctly infers an enum", () => {
+    expect(isEnum(NumberEnum)).toBeTruthy();
+    expect(isEnum(StringEnum)).toBeTruthy();
+    expect(isEnum({ A: "a", B: "b", C: "QWE" })).toBeTruthy();
+    expect(isEnum({ A: 0, 0: "A" })).toBeTruthy();
+  });
+  it("fails if stringEnum doesn't have value as key", () => {
+    expect(isEnum({ A: 0 })).toBeFalsy();
+    expect(isEnum([])).toBeFalsy();
+    expect(isEnum(Array)).toBeFalsy();
+    expect(isEnum(String)).toBeFalsy();
+    expect(isEnum({})).toBeFalsy();
+  });
+});
--- src/lib/isEnum.ts
+++ src/lib/isEnum.ts
@@ -1,3 +1,27 @@
-export const isEnum =
-  <T extends Record<string, string | number>>(enumValue: unknown) =>
-  (obj: unknown) => {};
+import { hasPropOf } from "./hasPropOf";
+import { isNumber } from "./isNumber";
+import { isObject } from "./isObject";
+import { isString } from "./isString";
+
+export const isEnum = <T extends Record<string, string | number>>(
+  target: unknown
+): target is T => {
+  if (!(target instanceof Array) && isObject(target)) {
+    const entries = Object.entries(target);
+    if (
+      entries.length !== 0 &&
+      entries.every(
+        ([key, value]) =>
+          typeof key === "string" &&
+          (typeof value === "string" || typeof value === "number") &&
+          ((isNumber(value) &&
+            hasPropOf(value, isString)(target) &&
+            target[value] === key) ||
+            isString(value))
+      )
+    ) {
+      return true;
+    }
+  }
+  return false;
+};

138674de14ca61d31f41c4dcf4fbc41358042666
Gabriel Rocha de Oliveira <rolivegab@gmail.com>
2023-05-09 13:44:36 -0300

    Improves EnumValueOf signature

--- src/isEnumValueOf.ts
+++ src/isEnumValueOf.ts
@@ -1,4 +1,4 @@
 export const isEnumValueOf =
-  <Target extends Record<string, string | number>>(e: Target) =>
+  <Target extends Record<string, unknown>>(e: Target) =>
   (target: unknown): target is Target[keyof Target] =>
     Object.values(e).some((value) => value === target);

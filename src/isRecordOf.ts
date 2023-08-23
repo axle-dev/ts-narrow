421e67c4e6fab79a2162c44585ea4067dbd3809d
Gabriel Rocha de Oliveira <rolivegab@gmail.com>
2023-08-23 08:50:14 -0300

    feat: improves isRecordOf types to keep keys typings

--- src/isRecordOf.ts
+++ src/isRecordOf.ts
@@ -3,5 +3,9 @@ import { NarrowOf } from "./types.js";
 
 export const isRecordOf =
   <Target>(f: NarrowOf<Target>) =>
-  (target: unknown): target is Record<string | number | symbol, Target> =>
+  (
+    target: unknown
+  ): target is typeof target extends Record<infer TKeys, any>
+    ? Record<TKeys, Target>
+    : Record<string | number | symbol, Target> =>
     isObject(target) && Object.values(target).every((i) => f(i));
